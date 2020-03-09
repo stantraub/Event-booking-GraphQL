1fef8bb4860cc9e780a2d28d5874dfc3aab85a7f
Stanley <stanley.traub@gmail.com>
2020-03-09 16:29:26 -0700

    Set up space model

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
-
+const mongoose = require('mongoose')
 const app = express()
 
 const events = []
@@ -63,4 +63,13 @@ app.use(
   })
 );
 
-app.listen(3000)
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-9taoe.mongodb.net/test?retryWrites=true&w=majority`
+  )
+  .then(() => 
+    app.listen(3000)
+    )
+  .catch(err => console.log(err)
+  );
+
---
+++ models/space.js
@@ -0,0 +1,150 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const spaceSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    cost: {
+        type: Number,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    website: {
+        type: String,
+        required: false
+     },
+    openHour: {
+        type: String,
+        required: true
+    },
+    closingHour: {
+        type: String,
+        required: true
+    },
+    neighborhood: {
+        type: String,
+        required: false
+    },
+    officeCapacity: {
+        type: Number,
+        required: false
+    },
+    people_capacity: {
+        type: Number,
+        required: false
+    },
+    availability: {
+        type: Boolean,
+        required: false
+    },
+    deskDay: {
+        type: Boolean,
+        required: false
+    },
+    sharedDesk: {
+        type: Number,
+        required: false
+    },
+    meetingRooms: {
+        type: Number,
+        required: false
+    },
+    hours24Access: {
+        type: Boolean,
+        required: false
+    },
+    transitStationMiles: {
+        type: Number,
+        required: false
+    },
+    wellnessRoom: {
+        type: Boolean,
+        required: false
+    },
+    phoneBooths: {
+        type: Number,
+        required: false
+    },
+    eventSpace: {
+        type: Boolean,
+        required: false
+    },
+    bikeParking: {
+        type: Boolean,
+        required: false
+    },
+    petFriendly: {
+        type: Boolean,
+        required: false
+    },
+    snacksDrinksIncluded: {
+        type: Boolean,
+        required: false
+    },
+    teaCoffeeIncluded: {
+        type: Boolean,
+        required: false
+    },
+    pingPong: {
+        type: Boolean,
+        required: false
+    },
+    billiards: {
+        type: Boolean,
+        required: false
+    },
+    foosball: {
+        type: Boolean,
+        required: false
+    },
+    showers: {
+        type: Boolean,
+        required: false
+    },
+    onsiteGym: {
+        type: Boolean,
+        required: false
+    },
+    printersIncluded: {
+        type: Boolean,
+        required: false
+    },
+    bocceBall: {
+        type: Boolean,
+        required: false
+    },
+    napRoom: {
+        type: Boolean,
+        required: false
+    }
+})
