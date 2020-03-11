29a3151e8185c7216374264329912333853afb93
Stanley <stanley.traub@gmail.com>
2020-03-11 14:36:07 -0700

    Finished navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+
+import NavBar from "./components/nav/navbar"
+import AuthPage from "./pages/Auth"
+import BookingsPage from "./pages/Bookings"
+import SpacePage from "./pages/Spaces"
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <React.Fragment>
+        <NavBar />
+        <main>
+          <Switch>
+            <Redirect from="/" to="/auth" exact />
+            <Route path="/" component={null} />
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/spaces" component={SpacePage} />
+            <Route path="/bookings" component={BookingsPage} />
+          </Switch>
+        </main>
+      </React.Fragment>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/nav/navbar.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css'
+
+// import {
+//   BrowserView,
+//   MobileView,
+//   isBrowser,
+//   isMobile
+// } from "react-device-detect";
+
+const NavBar = props => (
+  <div className="main-nav">
+    <span>
+      <Link to="/" className="main-logo">
+        Coworking
+      </Link>
+    </span>
+    <div className="action-buttons-wrapper">
+      <div className="action-item">
+        <span>List a workspace</span>
+      </div>
+      <div className="action-item">
+        <Link style={{ textDecoration: "none", color: "black" }} to={"/spaces"}>
+          <span>Find a workspace</span>
+        </Link>
+      </div>
+      <div className="action-item">
+        <span>Write a Review</span>
+      </div>
+    </div>
+  </div>
+);
+
+
+export default NavBar;
+
+
+// class Navbar extends React.Component {
+//     constructor(props) {
+//         super(props);
+
+//     }
+
+//     render() {
+//         if (isBrowser) {
+//             return (
+//             <div className="main-nav">
+//                 <span>
+//                     <Link to="/" className="main-logo">
+//                     Coworking
+//                     </Link>
+//                 </span>
+//                 <div className="action-buttons-wrapper">
+//                     <div className="action-item">
+//                     <span>List a workspace</span>
+//                     </div>
+//                     <div className="action-item">
+//                     <Link
+//                         style={{ textDecoration: "none", color: "black" }}
+//                         to={"/spaces"}
+//                     >
+//                         <span>Find a workspace</span>
+//                     </Link>
+//                     </div>
+//                     <div className="action-item">
+//                     <span>Write a Review</span>
+//                     </div>
+//                 </div>
+//             </div>
+        
+//             );
+//         } else {
+//             return (
+//               <div className="main-nav-mobile">
+//                 <div className="action-buttons-wrapper-mobile">
+//                   <Link to="/" className="main-logo-mobile">
+//                     Coworking
+//                   </Link>
+//                   <Link className="action-item-mobile"
+//                       style={{ textDecoration: "none", color: "black" }}
+//                       to={"/spaces"}
+//                   >
+//                       Find a workspace
+//                   </Link>
+//                 </div>
+//               </div>
+//             );
+//         }
+//     }
+// }
