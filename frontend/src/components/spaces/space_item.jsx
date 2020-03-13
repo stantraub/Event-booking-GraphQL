3735f3f1e0eea35e9219dd6883747cb47ef30303
Stanley <stanley.traub@gmail.com>
2020-03-12 20:46:27 -0700

    Access to object id

--- frontend/src/components/spaces/space_item.jsx
+++ frontend/src/components/spaces/space_item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './space_item.css'
 import { Link } from 'react-router-dom';
 import {
   BrowserView,
@@ -40,10 +41,27 @@ class SpaceItem extends React.Component {
 
   }
   render() {
-    console.log(this.props)
     return (
       <div className="space-item-wrapper">
-
+        <div className="space-item-info">
+          <div className="space-item-name">{this.props.name}</div>
+          <div className="space-item-amenities">
+            <div>
+              {this.props.city}, {this.props.state}
+            </div>
+            <span>{this.props.neighborhood}</span>
+            <div className="space-amenity-wrapper">
+              {this.includedAmenities()}
+            </div>
+            <div className="space-item-contact">
+              <div>{this.phone()}</div>
+              <div>{this.email()}</div>
+            </div>
+          </div>
+          <div className="space-item-cost">
+            <strong>${this.props.cost}</strong> per desk / month
+              </div>
+        </div>
       </div>
     );
   }
--- frontend/src/pages/Spaces.jsx
+++ frontend/src/pages/Spaces.jsx
@@ -1,27 +1,29 @@
 import React, { Component } from "react";
 
 import SpaceItem from "../components/spaces/space_item";
-import Spaces from '../components/spaces/space_index_container'
-// import {
-//   BrowserView,
-//   MobileView,
-//   isBrowser,
-//   isMobile
-// } from "react-device-detect";
+import './Spaces.css'
 const SpacesPage = ({spaces}) => {
   console.log(spaces)
   return (
     <div className="space-index-main">
       <div className="spaces-index-spaces-wrapper">
-        <div className="space-index-header">All spaces in San Francisco</div>
+        <div className="space-index-header">
+          All spaces in San Francisco
+        </div>
+        <div>
+          {spaces.map(({ _id, ...otherSpaceProps }, i) => {
+            return (
+              <SpaceItem 
+                key={i} 
+                spaceId={_id} 
+                {...otherSpaceProps}
+              />
+            )
+          })}
+        </div>
       </div>
-      <div>
-        {spaces.map(({ _id, ...otherSpaceProps }) => (
-          <SpaceItem key={_id} {...otherSpaceProps} />
-        ))}
-      </div>
+    
       <div className="space-index-map">
-        <h1> Hi </h1>
       </div>
     </div>
   );
